88e6dfe1f724b3c8bd3284049853297b6bdd213e
Robert Mischke <robertmischke@googlemail.com>
2011-06-15 10:43:40 +0200

    new nuget package dll

--- nuget.bat
+++ nuget.bat
@@ -1,5 +1,5 @@
 xcopy scr\Lib.Persistence\bin\Debug\Seedworks.Lib.* nuget\Seedworks.Persistence\lib\ /Y
-xcopy scr\Web.Lib.State\bin\Debug\Seedworks.Lib.* nuget\Seedworks.Web.State\lib\ /Y
+xcopy scr\Web.Lib.State\bin\Debug\Seedworks.* nuget\Seedworks.Web.State\lib\ /Y
 REM nuget pack nuget\Package.nuspec
 pause
 
