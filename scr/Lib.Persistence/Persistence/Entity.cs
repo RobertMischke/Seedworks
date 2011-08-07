6c122c4c24a32a524bb27d40b91a27007cdcba68
Robert Mischke <robertmischke@gmail.com>
2011-08-07 19:16:24 +0200

    new base type entity

---
+++ scr/Lib.Persistence/Persistence/Entity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Seedworks.Lib.Persistence
+{
+    /// <remarks>
+    /// Based on:
+    /// https://github.com/Slesa/Playground/blob/master/src/lib/DataAccess/DataAccess/DomainEntity.cs
+    /// </remarks>>
+    public class Entity : IEquatable<Entity>, IPersistable, WithDateModified
+    {
+
+        protected Entity()
+        {
+        }
+
+        protected Entity(int id)
+        {
+            Id = id;
+        }
+
+        public virtual int Id { get; set; }
+
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+
+        public virtual bool Equals(Entity other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id.Equals(default(int)) ? base.Equals(other) : other.Id.Equals(Id);
+        }
+
+        public override bool Equals(object obj)
+        {
+            return Equals(obj as Entity);
+        }
+
+        public override int GetHashCode()
+        {
+            return Id.Equals(default(int)) ? base.GetHashCode() : Id.GetHashCode();
+        }
+
+        public static bool operator ==(Entity left, Entity right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(Entity left, Entity right)
+        {
+            return !Equals(left, right);
+        }
+    }
+}
