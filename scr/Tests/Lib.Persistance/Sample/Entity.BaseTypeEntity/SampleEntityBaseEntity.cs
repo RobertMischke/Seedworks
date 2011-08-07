6e6cb129e885b3196daaab14ae377b4cc6a550d1
Robert Mischke <robertmischke@gmail.com>
2011-08-07 19:17:16 +0200

    prepared tests for entity

---
+++ scr/Tests/Lib.Persistance/Sample/Entity.BaseTypeEntity/SampleEntityBaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Seedworks.Lib.Persistence;
+
+namespace Seedworks.Tests.Lib.Persistence
+{
+    public class SampleEntityBaseEntity : IPersistable, WithDateModified
+    {
+        public virtual int Id { get; set; }
+        public virtual string Name { get; set; }
+
+        public virtual DateTime DateCreated { get; set; }
+        public virtual DateTime DateModified { get; set; }
+    }
+}
---
+++ scr/Tests/Lib.Persistance/Sample/Entity.BaseTypeEntity/SampleEntityBaseEntityRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NHibernate;
+using Seedworks.Lib.Persistence;
+
+namespace Seedworks.Tests.Lib.Persistence
+{
+    public class SampleEntityBaseEntityRepository : RepositoryDb<SampleEntityBaseEntity>
+    {
+        public SampleEntityBaseEntityRepository(ISession session)
+            : base(session)
+        {
+        }
+    }
+}
