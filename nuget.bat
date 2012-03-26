7a94bf6898610fac3238d0705c60d9705caf01a8
Robert Mischke <robertmischke@gmail.com>
2012-03-26 20:45:35 +0200

    Seedworks.Settings to nuget

--- nuget.bat
+++ nuget.bat
@@ -1,4 +1,5 @@
 xcopy scr\Lib.Persistence\bin\Debug\Seedworks.Lib.* nuget\Seedworks.Persistence\lib\ /Y
+xcopy scr\Lib.Settings\bin\Debug\Seedworks.* nuget\Seedworks.Settings\lib\ /Y
 xcopy scr\Web.Lib.State\bin\Debug\Seedworks.* nuget\Seedworks.Web.State\lib\ /Y
 REM nuget pack nuget\Package.nuspec
 pause
